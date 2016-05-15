5cb2b5fcd39437b73961c0559189c2ad7e3a56e3
Aaron Krebs <akrebs@ualberta.ca>
2016-05-14 18:06:58 -0600

    PlayerController handles player movement and colour.
    Player's material changes depending on x position.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public Material materialA;
+    public Material materialB;
+    private Renderer renderer;
+
+	// Use this for initialization
+	void Start () {
+        renderer = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+        transform.Translate(x, 0, z);
+        UpdateMaterial();
+	}
+
+    void UpdateMaterial()
+    {
+        if( transform.position[0] > 0)
+        {
+            renderer.material = materialA;
+        } else
+        {
+            renderer.material = materialB;
+        }
+    }
+}
