42bd50983c12064287e6d20aed42986ff11496da
Aaron Krebs <akrebs@ualberta.ca>
2016-05-14 18:10:50 -0600

    Code cleanup.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,33 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
     public Material materialA;
     public Material materialB;
-    private Renderer renderer;
+    private Renderer rend;
 
-	// Use this for initialization
-	void Start () {
-        renderer = GetComponent<Renderer>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         var x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
         var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
         transform.Translate(x, 0, z);
         UpdateMaterial();
-	}
+    }
 
     void UpdateMaterial()
     {
-        if( transform.position[0] > 0)
+        if (transform.position[0] > 0)
         {
-            renderer.material = materialA;
-        } else
+            rend.material = materialA;
+        }
+        else
         {
-            renderer.material = materialB;
+            rend.material = materialB;
         }
     }
 }
