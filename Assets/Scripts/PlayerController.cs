50f410c3462582d51a7100e4548141559348b878
Aaron Krebs <akrebs@ualberta.ca>
2016-05-14 18:37:48 -0600

    Add networked movement to players.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Networking;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : NetworkBehaviour
 {
 
     public Material materialA;
@@ -17,6 +18,10 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if( !isLocalPlayer )
+        {
+            return;
+        }
         var x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
         var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
         transform.Translate(x, 0, z);
