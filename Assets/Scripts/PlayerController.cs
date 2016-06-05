da6b0e268266d178074f66286ff53bcf9035fb3c
sganondorf <sgannon1@ualberta.ca>
2016-06-05 14:38:40 -0600

    Added in character art

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -31,6 +31,9 @@ public class PlayerController : NetworkBehaviour
         {
             return;
         }
+		if (Input.GetKey ("r")) {
+			rend.material = otherMaterial;
+		}
         var x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
         var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
         transform.Translate(x, 0, z);
