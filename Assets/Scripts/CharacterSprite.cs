3b4eb198cab953f96ec898ee0cda69e46a86dccb
Aaron Krebs <akrebs@ualberta.ca>
2016-07-24 16:11:01 -0600

    Scripts for assigning player numbers and setting sprites.

---
+++ Assets/Scripts/CharacterSprite.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class CharacterSprite : NetworkBehaviour {
+
+	public Sprite sprOne;
+	public Sprite sprTwo;
+	public Sprite monsterOne;
+	public Sprite monsterTwo;
+	public Sprite placeholder;
+
+	// player number (player 1, player 2, etc.)
+	[SyncVar]
+	private int playerNum;
+
+	private SpriteRenderer rend;
+
+	void Init() {
+		playerNum = 0;
+	}
+
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<SpriteRenderer>();
+	}
+
+	public override void OnStartServer()
+	{
+		UpdatePlayerNumber();
+	}
+
+	void Update() {
+		if( !isServer && !isLocalPlayer && IsPlayerOne() ) {
+			rend.sprite = monsterOne;
+		} else if ( !isServer && !isLocalPlayer && IsPlayerTwo() ) {
+			rend.sprite = monsterTwo;
+		} else if ( (isServer || isLocalPlayer) && IsPlayerOne() ) {
+			rend.sprite = sprOne;
+		} else if ( (isServer || isLocalPlayer) && IsPlayerTwo()) {
+			rend.sprite = sprTwo;
+		} else {
+			rend.sprite = placeholder;
+		}
+	}
+
+	private bool IsPlayerOne() {
+		return playerNum == 1;
+	}
+
+	private bool IsPlayerTwo() {
+		return playerNum == 2;
+	}
+
+	[Server]
+	private void UpdatePlayerNumber() {
+		GameObject obj = GameObject.FindWithTag("PlayerNumberManager");
+		if( obj == null ) {
+			Debug.Log ("Failed to get PlayerNumberManager.");
+		}
+		PlayerNumberManager manager = obj.GetComponent<PlayerNumberManager>();
+		playerNum = manager.GetPlayerNumber( netId );
+	}
+}
---
+++ Assets/Scripts/PlayerNumberManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerNumberManager : MonoBehaviour {
+
+	// maps from network ID to player number;
+	private Dictionary<NetworkInstanceId, int> player_numbers;
+
+	void Start() {
+		player_numbers = new Dictionary<NetworkInstanceId, int>();
+	}
+
+	public int GetPlayerNumber( NetworkInstanceId player )
+	{
+		if( player_numbers.ContainsKey( player ) )
+		{
+			return player_numbers[player];
+		}
+		player_numbers.Add( player, player_numbers.Count + 1 );
+		return player_numbers[player];
+	}
+}
