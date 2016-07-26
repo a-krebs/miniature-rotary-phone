bd1b0379bd5d360d1ada9f58e96f0be2c978a162
Aaron Krebs <akrebs@ualberta.ca>
2016-07-26 09:43:43 -0600

    Set object sprite from PlayerNumber.

---
+++ Assets/Scripts/ObjectSprite.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class ObjectSprite : NetworkBehaviour {
+
+	public Sprite sprOne;
+	public Sprite sprTwo;
+	public Sprite placeholder;
+
+	private SpriteRenderer rend;
+	private PlayerNumber playerNum;
+
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<SpriteRenderer>();
+		playerNum = PlayerNumber.GetLocalPlayerNumber();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (playerNum == null)
+		{
+			playerNum = PlayerNumber.GetLocalPlayerNumber();
+			return;
+		}
+		if (playerNum.IsPlayerOne()) {
+			rend.sprite = sprOne;
+		} else if (playerNum.IsPlayerTwo()) {
+			rend.sprite = sprTwo;
+		} else {
+			rend.sprite = placeholder;
+		}
+	}
+}
