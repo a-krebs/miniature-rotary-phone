bf6f586579311bcae8654dd280a343680477e1a7
Aaron Krebs <akrebs@ualberta.ca>
2016-07-26 09:42:30 -0600

    Grab player number on start.

--- Assets/Scripts/PlayerNumber.cs
+++ Assets/Scripts/PlayerNumber.cs
@@ -8,30 +8,65 @@ public class PlayerNumber : NetworkBehaviour {
 	[SyncVar]
 	private int playerNum;
 
-	void Init() {
+	void Awake () {
+		Debug.Log ("PlayerNumber Awake.");
 		playerNum = 0;
 	}
 
-	public override void OnStartServer()
-	{
+	public override void OnStartClient () {
+		Debug.Log ("PlayerNumber OnStartClient.");
+	}
+
+	public override void OnStartLocalPlayer () {
+		Debug.Log ("PlayerNumber OnStartLocalPlayer.");
+		gameObject.tag = "LocalPlayer";
+	}
+
+	public override void OnStartServer () {
+		Debug.Log ("PlayerNumber OnStartServer.");
 		UpdatePlayerNumber();
 	}
 
-	public bool IsPlayerOne() {
+	public bool IsPlayerOne () {
+		ThrowIfPlayerNumberNotSet();
 		return playerNum == 1;
 	}
 
-	public bool IsPlayerTwo() {
+	public bool IsPlayerTwo () {
+		ThrowIfPlayerNumberNotSet();
 		return playerNum == 2;
 	}
 
+	private void ThrowIfPlayerNumberNotSet() {
+		if (playerNum == 0) {
+			throw new System.MemberAccessException ("Player number not initialized.");
+		}
+	}
+
 	[Server]
-	private void UpdatePlayerNumber() {
+	private void UpdatePlayerNumber () {
+		Debug.Log ("PlayerNumber UpdatePlayerNumber.");
 		GameObject obj = GameObject.FindWithTag("PlayerNumberManager");
 		if( obj == null ) {
 			Debug.Log ("Failed to get PlayerNumberManager.");
 		}
 		PlayerNumberManager manager = obj.GetComponent<PlayerNumberManager>();
 		playerNum = manager.GetPlayerNumber( netId );
+		Debug.Log ("Updated player number. NetId: " + netId + ", PlayerNum: " + playerNum );
+	}
+
+	[Client]
+	public static PlayerNumber GetLocalPlayerNumber() {
+		GameObject player = GameObject.FindWithTag ("LocalPlayer");
+		if (player == null) {
+			throw new System.MemberAccessException ("Local player not found.");
+		}
+	
+		PlayerNumber num = player.GetComponent<PlayerNumber>();
+		if (num == null) {
+			throw new System.MemberAccessException ("Local player found, but has no PlayerNumber Component.");
+		}
+
+		return num;
 	}
 }
