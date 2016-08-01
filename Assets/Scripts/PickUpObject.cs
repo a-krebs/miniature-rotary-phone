43d0d938a8c914188c4b446a8bef83afe12584f4
Aaron Krebs <akrebs@ualberta.ca>
2016-08-01 13:50:33 -0600

    Add PickUpObject script. Add LocalPlayer tag.

---
+++ Assets/Scripts/PickUpObject.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+/// Objects that can be picked up and moved.
+///
+/// Attach to objects that players should be able to pick up.
+///
+/// Collisions are detected on the server. Client Authority is granted to
+/// the player that collides with the object. The client can then issue commands
+/// to pick up the object. When the authoritative player object leaves the
+/// collider, Client Authority is removed again.
+public class PickUpObject : NetworkBehaviour {
+
+	// only used on server
+	GameObject currentObject = null;
+
+	[SyncVar]
+	bool beingCarried = false;
+
+	void OnTriggerEnter2D (Collider2D other) {
+		if (!isServer) {
+			return;
+		}
+		if (beingCarried || currentObject != null) {
+			Debug.Log ("Object already being carried. Ignoring collision.");
+			return;
+		}
+		Debug.Log ("Object entered object trigger collider.");
+		GameObject gameObject = other.gameObject;
+		if (gameObject.tag != "Player" && gameObject.tag != "LocalPlayer") {
+			Debug.Log ("Object in collider is not a player.");
+			return;
+		}
+
+		currentObject = gameObject;
+		GetComponent<NetworkIdentity>().AssignClientAuthority( currentObject.GetComponent<NetworkIdentity>().connectionToClient );
+	}
+	
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (!isServer)
+		{
+			Debug.Log ("Not server. Ignoring collision.");
+			return;
+		}
+		if (beingCarried) {
+			Debug.Log ("Object already being carried. Ignoring collision.");
+			return;
+		}
+		if (other.gameObject == currentObject) {
+			Debug.Log ("Player exited object trigger collider.");
+			GetComponent<NetworkIdentity>().RemoveClientAuthority( currentObject.GetComponent<NetworkIdentity>().connectionToClient );
+			currentObject = null;
+		}
+	}	
+	
+	void Update()
+	{
+		if (!hasAuthority) {
+			return;
+		}
+		if (beingCarried)
+		{
+			if (Input.GetMouseButtonDown(0))
+			{
+				// local version of the object is authoritative,
+				// so remove the local transform
+				transform.parent = null;
+				CmdPutDown();
+			}
+		}
+		else
+		{
+			if(Input.GetMouseButtonDown(0))
+			{
+				CmdPickUp();
+				// local version of the object is authoritative,
+				// so use local transform
+				transform.parent = PlayerNumber.GetLocalPlayerGameObject().transform;
+			}
+		}
+	}
+
+	[Command]
+	void CmdPickUp()
+	{
+		Debug.Log ("Picking up.");
+		if (currentObject == null) {
+			throw new System.MemberAccessException ("Invalid state. Cannot pick up.");
+		}
+		beingCarried = true;
+	}
+
+	[Command]
+	void CmdPutDown()
+	{
+		Debug.Log ("Putting down.");
+		if (!beingCarried || currentObject == null) {
+			throw new System.MemberAccessException ("Invalid state. Cannot put down.");
+		}
+		beingCarried = false;
+	}
+}
