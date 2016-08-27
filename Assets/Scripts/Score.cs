37b3bc559b7760879bc761b50b87164a2fd6299b
sganondorf <sgannon1@ualberta.ca>
2016-08-27 13:37:41 -0600

    Added score/completion bar and script to change scorebar value

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Score : NetworkBehaviour {
+
+	public int score;                           
+	public Slider scoreSlider;                                 
+
+
+
+	void Awake ()
+	{
+		score = 0;
+	}
+
+
+	public void ChangeScore (int amount)
+	{
+
+
+		score += amount;
+
+
+		scoreSlider.value = score;
+
+
+	}
+		     
+}
