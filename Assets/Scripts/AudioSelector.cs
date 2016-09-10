d953a35da76df3c4238108298651f0aa7cf647dd
Aaron Krebs <akrebs@ualberta.ca>
2016-09-10 11:34:20 -0600

    Added audio tracks.

---
+++ Assets/Scripts/AudioSelector.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioSelector : MonoBehaviour {
+
+	public AudioClip trackOne;
+	public AudioClip trackTwo;
+
+	private PlayerNumber playerNum;
+	private AudioSource source;
+
+	void Start () {
+		source = GetComponent<AudioSource>();
+		playerNum = PlayerNumber.GetLocalPlayerNumber();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if( playerNum == null ) {
+			playerNum = PlayerNumber.GetLocalPlayerNumber();
+			return;
+		}
+		if( source.isPlaying ) {
+			return;
+		} else {
+			if( playerNum.IsPlayerOne() ) {
+				source.clip = trackOne;
+			} else {
+				source.clip = trackTwo;
+			}
+			source.Play();
+		}
+	
+	}
+}
