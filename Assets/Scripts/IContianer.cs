fe4d38a673e75552a11e727662c9e2413032fdc8
Aaron Krebs <akrebs@ualberta.ca>
2016-09-21 09:46:26 -0600

    Add a container interface to use for Boxes and Slots.

---
+++ Assets/Scripts/IContianer.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+/// Container that can hold one or more PickUpObjects.
+public interface IContainer
+{
+	/// Get the number of PickUpObjects currently in the container.
+	int Count { get; }
+
+	/// Get the max capacity of the container.
+	int Capacity { get; }
+
+	/// Get a PickUpObject from the container.
+	/// Return null if container is empty.
+	PickUpObject Get(Transform parent, NetworkRequest.Result handler);
+
+	/// Put the given PickUpObject in the container.
+	/// Throw an exception if the container is full.
+	void Put(PickUpObject obj);
+}
