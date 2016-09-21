dc4b241737a52a01a14759af32d74f1923ee2c6d
Aaron Krebs <akrebs@ualberta.ca>
2016-09-21 12:46:13 -0600

    Implement IContainer on BoxContainer.

--- Assets/Scripts/BoxContainer.cs
+++ Assets/Scripts/BoxContainer.cs
@@ -23,11 +23,34 @@ public class BoxContainer : NetworkBehaviour, IContainer
 
 	public PickUpObject Get(Transform parent, NetworkRequest.Result handler)
 	{
-		return null;
+		if (Count < 1)
+		{
+			Debug.Log("BoxContainer empty.");
+			return null;
+		}
+		GameObject child = transform.GetChild(0).gameObject;
+		child.transform.position = parent.position;
+		child.transform.parent = parent;
+		child.SetActive(true);
+		// TODO null check
+		return child.GetComponent<PickUpObject>();
 	}
 
 	public void Put(PickUpObject obj)
 	{
-		throw new System.NotImplementedException();
+		if (Count >= Capacity)
+		{
+			Debug.Log("Slot not empty.");
+			throw new System.MemberAccessException();
+		}
+		obj.transform.position = transform.position;
+		obj.transform.parent = transform;
+	}
+
+	void Update() {
+		for(int i = 0; i < transform.childCount; ++i) {
+			GameObject obj = transform.GetChild(i).gameObject;
+			obj.SetActive(false);
+		}
 	}
 }
