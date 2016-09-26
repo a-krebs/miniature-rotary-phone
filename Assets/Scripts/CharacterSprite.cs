81f0c96ec1f7fb4f64ac23c3c2ab14c95511359b
sgannon1 <sgannon1@ualberta.ca>
2016-09-25 18:21:17 -0600

    Coloured character, background art added & animations redone
    - Coloured background and character art added
    - Different backgrounds for player 1 and player 2
    - Sound FX in audio folder but not yet in game
    - Animations fixed

--- Assets/Scripts/CharacterSprite.cs
+++ Assets/Scripts/CharacterSprite.cs
@@ -9,16 +9,20 @@ public class CharacterSprite : NetworkBehaviour {
 	public Sprite sprTeeth;
 	public Sprite sprBurb;
 	public Sprite placeholder;
+    public Sprite greenBackground;
+    public Sprite pinkBackground;
 
 	private SpriteRenderer rend;
 	private PlayerNumber playerNum;
 	private Animator animator;
+    public SpriteRenderer background;
 
 	// Use this for initialization
 	void Start () {
 		rend = GetComponent<SpriteRenderer>();
 		playerNum = GetComponent<PlayerNumber>();
 		animator = GetComponent<Animator>();
+        background = GameObject.Find("env_room_background").GetComponent<SpriteRenderer>();
 	}
 
 	void Update() {
@@ -29,9 +33,11 @@ public class CharacterSprite : NetworkBehaviour {
 			rend.sprite = sprTeeth;
 			animator.runtimeAnimatorController = Resources.Load("Animation/teethlegs") as RuntimeAnimatorController;
 		} else if ( isLocalPlayer && IsPlayerOne() ) {
+            background.sprite = pinkBackground;
 			rend.sprite = sprGal;
 			animator.runtimeAnimatorController = Resources.Load("Animation/gal") as RuntimeAnimatorController;
 		} else if ( isLocalPlayer && IsPlayerTwo()) {
+            background.sprite = greenBackground;
 			rend.sprite = sprDude;
 			animator.runtimeAnimatorController = Resources.Load("Animation/dude") as RuntimeAnimatorController;
 		} else {
