67c298b090ed36917419ab7b3b740651d4df0c32
Aaron Krebs <akrebs@ualberta.ca>
2016-10-22 13:02:48 -0600

    Revert "Mapped xbox controller input; closes #16"
    This reverts commit ae83327b03ef8f4f550a1e2a2132ce37ba410f85.

--- Assets/Scripts/NetworkManagerStartup.cs
+++ Assets/Scripts/NetworkManagerStartup.cs
@@ -51,7 +51,7 @@ public class NetworkManagerStartup : MonoBehaviour {
 
 	void Update() {
 		Network.TestConnection (true);
-		if(Input.GetKeyDown("joystick button 10")){
+		if(Input.GetKeyDown(KeyCode.R)){
 			NetworkManager.Shutdown ();
 			SceneManager.LoadScene(0); 
 		}
