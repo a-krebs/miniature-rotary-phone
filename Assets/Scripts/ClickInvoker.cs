ae5642a2b92c4c365760e4ae6726ff34a530e047
sganondorf <sgannon1@ualberta.ca>
2016-10-24 22:29:31 -0600

    Added script to invoke start button click

---
+++ Assets/Scripts/ClickInvoker.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ClickInvoker : MonoBehaviour {
+
+	public Button button;
+
+	void Update(){
+		if(Input.GetKeyDown("joystick button 9")){
+			button.onClick.Invoke ();
+		}
+	}
+}
--- Assets/Scripts/LoadSceneOnClick.cs
+++ Assets/Scripts/LoadSceneOnClick.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 public class LoadSceneOnClick : MonoBehaviour
 {
 
-    public void LoadByIndex(int sceneIndex)
+	public void LoadByIndex(int sceneIndex)
     {
         SceneManager.LoadScene(sceneIndex);
     }
