2061ea824d187b37d03f3e9d7cea417afd3c87b2
Aaron Krebs <akrebs@ualberta.ca>
2016-10-25 20:02:01 -0600

    InputManager class.

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+
+public class InputManager {
+
+	public static bool PickUpPutDownPressed() {
+		return false;
+	}
+
+	public static bool ResetPressed() {
+		return false;
+	}
+
+	public static bool ChangeSelectionPressed() {
+		return false;
+	}
+
+	public static bool StartGamePressed() {
+		return false;
+	}
+}
